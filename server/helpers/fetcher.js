e8880e521ff320852b2962a0edcd3a3523950836
Matteo Maronati <matteo@gentleman-studio.de>
2016-11-20 16:30:58 +0100

    Up: rewriting fetch functions in separate class

---
+++ server/helpers/fetcher.js
@@ -0,0 +1,58 @@
+'use strict'
+
+// =========================================
+// Fetcher
+// =========================================
+
+let tor = require('tor-request')
+let exec = require('child_process').exec;
+let util = require('util')
+let env = require('../.env.json')
+let EventEmitter = require('events').EventEmitter
+
+
+tor.TorControlPort.password = env.tor.pwd
+
+class Fetcher extends EventEmitter {
+
+  constructor () {
+    super()
+  }
+
+  // makes request to apify to get current exit node
+  geLocalizeIp () {
+    tor.request({
+      url:'http://ip-api.com/json',
+      setTimeout: 6000
+    },
+    (err, res, body) =>  {
+      // if everything is fine
+      if (!err && res.statusCode == 200) {
+        // parse response and prepare object to be stored
+        let jsonBody = JSON.parse(body)
+        let entry = {
+          lat: jsonBody.lat,
+          lon: jsonBody.lon,
+          ip: jsonBody.query,
+          time: new Date
+        }
+        this.emit('fetched', false, entry)
+      }
+      else {
+        this.emit('fetched', err, false)
+      }
+    })
+    .on('timeout', (err) => {
+      this.emit('timeout', err, false)
+    })
+  }
+
+  requestNewExitNode (cb) {
+    tor.renewTorSession((done) => {
+      this.emit('newExitNode')
+    })
+  }
+
+}
+
+module.exports = new Fetcher()
--- server/index.js
+++ server/index.js
@@ -4,61 +4,23 @@
 // Mapping tor
 // =========================================
 
-let tor = require('tor-request')
-let request = require('request')
-let exec = require('child_process').exec;
-let util = require('util')
-let Logger = require('./helpers/logger')
-let env = require('./.env.json')
+let logger = require('./helpers/logger')
+let fetcher = require('./helpers/fetcher')
 
 
-tor.TorControlPort.password = env.tor.pwd
+class MappingTor {
 
-class MappingTor extends Logger {
-
-  constructor () {
-    super()
-  }
-  // makes request to apify to get current exit node
-  makeRequest () {
-    tor.request({
-      url:'http://ip-api.com/json',
-      setTimeout: 6000
-    },
-    (err, res, body) =>  {
-      // if everything is fine
-      if (!err && res.statusCode == 200) {
-        // parse response and prepare object to be stored
-        let jsonBody = JSON.parse(body)
-        let entry = {
-          lat: jsonBody.lat,
-          lon: jsonBody.lon,
-          ip: jsonBody.query,
-          time: new Date
-        }
-        // write log entry
-        this.write(entry, './server/log.json', () => { 
-          this.requestNewExitNode()
-        })
-      }
-      else {
-        this.requestNewExitNode()
-        console.log(err, res)
-      }
-    })
-    .on('timeout', (err) => {
-      console.log(err)
-    })
+  init () {
+    fetcher.geLocalizeIp()
+    this.addListeners()
   }
 
-  requestNewExitNode () {
-    tor.renewTorSession((done) => {
-      this.makeRequest()
+  addListeners () {
+    fetcher.on('fetched', (err, entry) => {
+      if (!err) {
+        console.log(entry)
+      }
     })
   }
 
 }
-
-let mapTor = new MappingTor()
-
-mapTor.makeRequest()
