46dac12b54703145cae1cc23f8cdc6da13ddeeac
Matteo Maronati <matteo@gentleman-studio.de>
2016-11-21 20:54:08 +0100

    Up: first successful mongo connection

--- server/helpers/logger.js
+++ server/helpers/logger.js
@@ -3,21 +3,34 @@
 let EventEmitter = require('events').EventEmitter
 let fs = require('fs')
 
+let Mongodb = require('mongodb').MongoClient
+let env = require('../.env.json')
+
+// Connection URL
+
 let jsonfile = require('jsonfile')
 
 class Logger extends EventEmitter {
 
   constructor () {
     super()
+
+    this.testDbConenction()
+
   }
 
   write (entry, file) {
-    jsonfile.readFile(file, (err, obj) => {
-      obj.push(entry)
-      jsonfile.writeFile(file, obj, {spaces: 2}, (err) => {
-        this.emit('written')
-      })
+
+  }
+
+
+  testDbConenction () {
+
+    Mongodb.connect(env.mongoDb.endpoint, function(err, db) {
+      console.log("Connected successfully to mongo db")
+      db.close()
     })
+
   }
 
 
