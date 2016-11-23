4541bf8d4699bb7db56049b9ba3c725721bab5e9
Matteo Maronati <matteo@gentleman-studio.de>
2016-11-23 19:31:20 +0100

    Up: implemnted new applciation mode enviromental variable

--- server/index.js
+++ server/index.js
@@ -7,20 +7,36 @@
 let fetcher = require('./helpers/fetcher')
 let server = require('./helpers/server')
 let db = require('./helpers/db')
+let env = require('./.env.json')
+
+console.log(env.application);
 
 class MappingTor {
 
   init () {
     // fetcher.getLocalizeIp()
     // this.addListeners()
-    server.init()
-    db.insertCollection('locations', (collection) => {
-      console.log(collection)
-    })
+    console.log(process.env);
 
-    db.deleteCollection('locations', (collection) => {
-      console.log(collection)
-    })
+
+    // Switch collection base on application mode
+    // use this to avoid always overriding the clean location
+    if (env.application.mode === 'development') {
+      this.collection = 'locations-dev'
+    } else {
+      this.collection = 'locations'
+    }
+
+    server.init()
+    // db.insertDevCollection( (collection) => {
+    //   console.log(collection)
+    // })
+    // db.readCollection('locations', (collection) => {
+    //   console.log(collection)
+    // })
+    // db.deleteCollection('locations', (collection) => {
+    //   console.log(collection)
+    // })
   }
 
 
@@ -56,8 +72,10 @@ class MappingTor {
     db.readCollection('locations', (collection) => {
       let check = _.find(cleanLocations, (o) => {  return o.ip === entry.ip;})
       // if entry does not exist
-      if (typeof check !=== 'undefined') {
-        db.insertCollection(entry)
+      if (typeof check !== 'undefined') {
+        db.insertCollection(entry, (resul) => {
+          console.log(result);
+        })
       }
     })
   }
