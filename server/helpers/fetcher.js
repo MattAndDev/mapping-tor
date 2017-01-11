aa73bb088b1731f90fb67204c05aece92e7b43f2
Matteo Maronati <matteo@gentleman-studio.de>
2017-01-11 18:18:47 +0100

    Rw: better structure for Fetcher class

--- server/helpers/fetcher.js
+++ server/helpers/fetcher.js
@@ -20,15 +20,27 @@ class Fetcher extends EventEmitter {
 
   constructor () {
     super()
-    this.requestNodeInterval = 60000
+
+    // configure throttle for new exit nodes
+    this.requestNodeInterval = 60000 // 1 minute
   }
 
-  // makes request to apify to get current exit node
-  getLocalizeIp () {
+  // init function
+  init () {
+    // startby schedulign first new exit nodes
+    this.scheduleNewExitNode()
+  }
+
+  // Ip geo localizer
+  // ==
+  // makes request to apify to get current exit node data
+  // emits a 'fetched' event, passing either error or result
+
+  _getLocalizeIp () {
 
     tor.request({
       url:'http://ip-api.com/json',
-      timeout: 1000
+      timeout: 5000
     },
     (err, res, body) =>  {
       // if everything is fine
@@ -49,23 +61,54 @@ class Fetcher extends EventEmitter {
     })
   }
 
+  // Schedule new exit node request
+  // ==
+  // Time based request for new exit node
+  // if called before timeout elapsed
+  // takes care of waiting correct amount of time
+
   scheduleNewExitNode () {
+
+    // local current time
     let now = Date.now()
+
     if (this.lastNodeRequest && now - this.requestNodeInterval < this.lastNodeRequest ) {
+      // if last request exist and not enoug htime passed
+      // calculate missing milliseconds and fire setTimeout
       let timeout = this.lastNodeRequest - (now - this.requestNodeInterval)
       setTimeout(() => {
-        this.requestNewExitNode()
+        this._requestNewExitNode()
       }, timeout)
+
     } else {
-      this.requestNewExitNode()
+      // enough time has passed, request new node
+      this._requestNewExitNode()
     }
     this.lastNodeRequest = Date.now()
   }
 
-  requestNewExitNode () {
-    // Set tor env variables
+
+  // Request new TOR exit node via control port
+  // ==
+  // uses tor-request helper to arbitrarily request a new exit node
+  // when new exit nod is there, calls geoLocalizing function
+
+  _requestNewExitNode () {
+
+    // call the tor request helper
     tor.renewTorSession((done) => {
-      this.emit('newExitNode')
+
+      if (!done) {
+        // if done is null, new node is there
+        // geo localize the ip
+        this._getLocalizeIp()
+      }
+      else {
+        // something went wrong
+        // some error handling should be done here
+        // probably some configuration is wrong
+        process.exit(1);
+      }
     })
   }
 
